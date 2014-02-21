b4d04d08340f3478d1ddd569cec46500f434ee7f
Matthew Crouch <matthew.a.crouch@gmail.com>
2014-02-21 17:53:39 -0500

    added seed data

--- gotta_go_rails/db/seeds.rb
+++ gotta_go_rails/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+[Restroom, Sensor, Activity].each(&:delete_all)
+
+photocell_restroom = Restroom.create!({name: "Photocell Restroom"})
+photocell_restroom.sensors.create!({name: "Photocell Sensor", token: "photocell"})
+
+pir_restroom = Restroom.create!({name: "PIR Restroom"})
+pir_restroom.sensors.create!({name: "PIR Sensor", token: "pir"})
+
+ir_restroom = Restroom.create!({name: "IR Restroom"})
+ir_restroom.sensors.create!({name: "IR Sensor", token: "ir"})
+
+sonar_restroom = Restroom.create!({name: "Sonar Restroom"})
+sonar_restroom.sensors.create!({name: "Sonar Sensor", token: "sonar"})
